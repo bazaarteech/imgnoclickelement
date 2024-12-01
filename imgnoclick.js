7df6e2f66b0e6357c946603814e9bc55d743f80c
bazaarteech <snapreview491@gmail.com>
2024-12-01 18:10:53 +0100

    imgnoclick.js

---
+++ imgnoclick.js
@@ -0,0 +1,18 @@
+document.addEventListener('contextmenu', function(e) {
+
+            if (e.target.tagName === 'IMG') {
+
+                e.preventDefault();
+
+            }
+
+        });
+ document.addEventListener('dragstart', function(e) {
+
+            if (e.target.tagName === 'IMG') {
+
+                e.preventDefault();
+
+            }
+
+        });
